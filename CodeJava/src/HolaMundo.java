fdbad7da316820dc071762e383dc49daf86fef45
drivas022 <115203434+drivas022@users.noreply.github.com>
2025-04-09 12:17:07 -0600

    Reto - Presentate con Java.

---
+++ CodeJava/src/HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+    // Agregamos el metodo principal (main)
+    //psvm
+    public static void main(String[] args) {
+        //sout - Atajo para imprimir información a consola.
+        System.out.println("Hola Mundo con Java");
+    }
+}
---
+++ CodeJava/src/Presentate.java
@@ -0,0 +1,9 @@
+import javax.sound.midi.Soundbank;
+
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Hola soy Diego");
+        System.out.println("Edad: 21");
+        System.out.println("Guatemala");
+    }
+}
