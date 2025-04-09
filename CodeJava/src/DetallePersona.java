641ceaf01f5ce2fb04eb65cc8329ceed3b16dde1
drivas022 <115203434+drivas022@users.noreply.github.com>
2025-04-09 13:26:04 -0600

    Detalle Persona.

---
+++ CodeJava/src/DetallePersona.java
@@ -0,0 +1,16 @@
+public class DetallePersona {
+    public static void main(String[] args) {
+        String nombre = "Diego Rivas";
+        int edad = 21;
+        double altura = 1.78;
+        String paisOrigen = "Guatemala";
+        char EstadoCivil = 'C'; //C - Casado | S - Soltero
+
+        //Imprimiendo
+        System.out.print(nombre);
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(paisOrigen);
+        System.out.println(EstadoCivil);
+    }
+}
