9bdbb36f4a693be7fe19048fe9cf16013afeed0e
drivas022 <115203434+drivas022@users.noreply.github.com>
2025-04-21 10:49:22 -0600

    Ejercico basico 'ConcatenaciónCadenas'

---
+++ CodeJava/src/ConcatenacionCadenas.java
@@ -0,0 +1,9 @@
+public class ConcatenacionCadenas {
+    public static void main(String[] args) {
+        System.out.println("*** Concatencion de Cadenas ***");
+        var nombre = "Diego";
+        var apellido = "Rivas";
+        var nombreCompleto = nombre + " " + apellido;
+        System.out.println("Nombre completo: " + nombreCompleto);
+    }
+}
