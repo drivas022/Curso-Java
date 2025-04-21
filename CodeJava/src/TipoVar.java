c7ade4728f688be7d95811581df1a379c3d274aa
drivas022 <115203434+drivas022@users.noreply.github.com>
2025-04-21 10:42:18 -0600

    Utilización de var

---
+++ CodeJava/src/TipoVar.java
@@ -0,0 +1,29 @@
+public class TipoVar {
+    public static void main(String[] args) {
+        System.out.println("*** Uso de var en Java ***");
+        //Sin el uso de var
+        String nombre1 = "Juan";
+        System.out.println("nombre1: " + nombre1);
+        //Con el uso de var
+        var nombre2 = "Carlos"; //las variables "var" son variables locales, lo cual no pueden ser globales.
+        System.out.println("nombre2: " + nombre2);
+        //Definir otras variables usando var
+        var edad = 30; //va a inferir el tipo dependiendo el valor que se este asigando, en este caso int.
+        System.out.println("edad = " + edad);
+        var sueldo = 5000.5F; //se infiere el tipo float
+        System.out.println("sueldo: " + sueldo);
+
+        //Ejemplos
+        var esCasado = false; //Se infiere tipo boolean
+        esCasado = true;
+        //esCasado = "NO"; No podemos asigar un tipo distinto
+        var numero = 30L; //se infiere tipo Long
+
+        //Se debe definir su valor | A CONTINUACIÓN SE MUESTRA UN EJEMPLO ERRONEO
+        var precio; //aqui se debe asignar un valor desde su declaración
+        precio = 10;
+
+        //Se debe poder inferir el tipo de dato
+        var apellido = null; //no es inferible
+    }
+}
