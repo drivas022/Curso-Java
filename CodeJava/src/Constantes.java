c205b033562173bf675c5fef59df1ad38f3470e8
drivas022 <115203434+drivas022@users.noreply.github.com>
2025-04-21 11:06:40 -0600

    Uso de 'Constantes'

---
+++ CodeJava/src/Constantes.java
@@ -0,0 +1,18 @@
+public class Constantes {
+    public static void main(String[] args) {
+        //SE USA LA PALABRA "FINAL" PARA DECLARAR UNA CONSTANTE
+        //PARA DECLARAR CONSTANTES SE COLOCA EN MAYUSCULAS Y SI HAY ESPAICOS SE USA _
+        System.out.println("*** Constantes ***");
+        final var DIAS_SEMANA = 7;
+        System.out.println("DIAS_SEMANA = " + DIAS_SEMANA);
+        final var PI = 3.1416;
+        System.out.println("PI = " + PI);
+        //Constates definidas poor Java
+        System.out.println("Math.PI " + Math.PI);
+
+        final var MENSAJE_BIENVENIDA = "Bienvenid@ a la Universidad de Java";
+        System.out.println("MENSAJE_BIENVENIDA = " + MENSAJE_BIENVENIDA);
+        final var MINUTOS_POR_SEGUNDO = 60;
+        System.out.println("MINUTOS_POR_SEGUNDO = " + MINUTOS_POR_SEGUNDO);
+    }
+}
