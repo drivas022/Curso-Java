efabc31bcdd5e883f15289ebbd2b4aad9b85d3a6
drivas022 <115203434+drivas022@users.noreply.github.com>
2025-10-22 11:43:37 -0600

    Ciclo For - Numeros Pares

---
+++ Ciclos/src/NumerosParesFor.java
@@ -0,0 +1,9 @@
+public class NumerosParesFor {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 20; i++){
+            if (i % 2 == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
