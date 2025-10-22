ed0d8581824d54abb29b3fc644a71c97fb982a8b
drivas022 <115203434+drivas022@users.noreply.github.com>
2025-10-22 12:02:05 -0600

    Ejercicio Suma Acumulativa

--- Ciclos/src/NumerosParesFor.java
+++ Ciclos/src/NumerosParesFor.java
@@ -1,5 +1,6 @@
 public class NumerosParesFor {
     public static void main(String[] args) {
+        System.out.println("*** Números pares usando Ciclo For ***");
         for(int i = 0; i <= 20; i++){
             if (i % 2 == 0){
                 System.out.print(i + " ");
