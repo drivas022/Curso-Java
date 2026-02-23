adc9322bd25464255ce55939bfcbd9b565612492
Diego Rivas <drivas022@yahoo.com>
2026-02-23 16:22:26 -0600

    feat: Matrix syntaxis

---
+++ Matrices/src/Matrices.java
@@ -0,0 +1,18 @@
+public class Matrices {
+    public static void main(String[] args) {
+        // Definimos una matriz
+        int[][] matrix = new int[2][3]; // 2 filas y 3 columnas
+        var matriz = new int [2][3];
+        // Modificar valores de la matriz
+        matriz[0][0] = 100;
+        matriz[0][1] = 200;
+        matriz[0][2] = 300;
+        matriz[1][0] = 400;
+        matriz[1][1] = 500;
+        matriz[1][2] = 600;
+        // Acceder a los valores
+        System.out.println("Valor 1 [0][0] = " + matriz[0][0]);
+
+
+    }
+}
