611978eb6759c0d682093d5fdf64552eb553bb90
Diego Rivas <drivas022@yahoo.com>
2026-02-23 12:59:37 -0600

    feat: Adding new Array file

---
+++ Arreglos/src/Arreglos.java
@@ -0,0 +1,12 @@
+public class Arreglos {
+    public static void main(String[] args) {
+        // Arreglos
+        // 1. Declaramos un arreglo
+        int[] enteros;
+        // 2. Inicializar un arreglo
+        enteros = new int[3];
+        // Declarar e inicializar un arreglo
+        //int[] numeros = new int[2];
+        var numeros = new int[2];
+    }
+}
