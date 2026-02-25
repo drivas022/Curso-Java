5e0369f455996332fab1d9aad38ece1f99d7bb04
Diego Rivas <drivas022@yahoo.com>
2026-02-24 18:02:47 -0600

    feat: Using methods

---
+++ Funciones/src/Funciones.java
@@ -0,0 +1,9 @@
+public class Funciones {
+    static void Saludar(String mensaje){
+        System.out.println("Mensaje: " + mensaje);
+    }
+    public static void main(String[] args) {
+        Saludar("Hola desde Java");
+        Saludar("Adios");
+    }
+}
